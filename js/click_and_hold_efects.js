48bb710689224980170c678daf415cc63f02f5ea
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2019-10-31 19:11:09 -0500

    Move a div with id = cursor when move the mouse

--- js/click_and_hold_efects.js
+++ js/click_and_hold_efects.js
@@ -1,1 +1,11 @@
+let cursor = document.getElementById('cursor');
+
+window.onmousemove = function (e) {
+  let x = e.clientX;
+  let y = e.clientY;
+
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
+  console.log(cursor.style);
+}
 
